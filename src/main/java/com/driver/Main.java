a8465a2c89b443f099e411b21ab11102162ed820
Azizul <you@example.comazizuljohn121454@gmail.com>
2022-10-08 00:56:25 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly r = new RWOnly();
+       // r.name="Azizul"; //----> this is not possible bcz name var is private, scope is only the local class
+        r.setName("Azizul");
+        System.out.println(r.getName());
+    }
 }
